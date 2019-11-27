a337b1c6bf61f6c8b2e092735646c781d0b71e9a
Val <valentinedestrait@hotmail.com>
2019-11-27 14:06:03 +0100

    footer + scripts

---
+++ view/pages/index.php
@@ -0,0 +1,8 @@
+<?php
+include '../partials/header.php';
+?>
+<h1>Gift Box</h1>
+
+<?php
+include '../partials/footer.php';
+?>
---
+++ view/partials/footer.php
@@ -0,0 +1,10 @@
+<footer></footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+crossorigin="anonymous"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="../../assets/js/index.js"></script>
+</body>
+</html>
