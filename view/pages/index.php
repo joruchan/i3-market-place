39f7e74ffbacbf956d739c80bcfbe2d6e584b1ed
Jordane <jordane.frechet@gmail.com>
2019-11-27 16:53:36 +0100

    creation jsons

--- view/pages/index.php
+++ view/pages/index.php
@@ -2,11 +2,32 @@
 include '../partials/header.php';
 ?>
 
-<main>
-<div class="container">
-    <h1>Gift Box</h1>
-</div>
-
+<main class="container-main">
+	<div class="row content-main">
+		<div class="col s12 m3 category-block">
+			<p>Categories</p>
+			<div id="category-list">
+				
+			</div>
+		</div>
+		<div class="col s12 m9 objects-block">
+			<p>Current Listings</p>
+			<div id="product-list">
+				<div class="col s12 m4">
+					<div class="card">
+						<div class="card-image">
+							<img src="https://robohash.org/placeholder.png">
+							<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+						</div>
+						<div class="card-content">
+							<span class="card-title">Card Title</span>
+							<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 
 
