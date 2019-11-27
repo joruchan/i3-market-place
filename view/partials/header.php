58d1f1fb2ed7630e9f1b14374aba7b6fc182c7c9
Jordane <jordane.frechet@gmail.com>
2019-11-27 14:57:23 +0100

    Header v1

--- view/partials/header.php
+++ view/partials/header.php
@@ -6,6 +6,38 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" href="../../assets/css/style.css">
+	<script
+  		src="https://code.jquery.com/jquery-3.4.1.min.js"
+  		integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+		crossorigin="anonymous">
+	</script>
+	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<title>i3 GiftBox</title>
 </head>
 <body>
+	<header>
+		<nav>
+			<div class="navbar pink lighten-3">
+				<a href="#!" class="logo">Logo</a>
+				<ul class="right hide-on-med-and-down">
+					<li>
+						<form action="">
+							<div class="input-field">
+								<input id="search" type="search" required>
+								<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+								<i class="material-icons">close</i>
+							</div>
+						</form>
+					</li>
+					<li>
+						<a class="waves-effect waves-light btn pink accent-4">
+							<i class="material-icons left">face</i>
+							Profile
+						</a>
+					</li>
+				</ul>
+		    </div>
+	  	</nav>
+	</header>
+	
