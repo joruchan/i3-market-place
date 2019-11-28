bc4395914e53e18e4c9f9ad2402e08c1dade9e85
CeriseDW <CarnetsDeCerise@gmail.com>
2019-11-28 22:14:20 +0100

    ajout img par default sur product

--- view/dataDB/addProducts.php
+++ view/dataDB/addProducts.php
@@ -19,12 +19,18 @@ $cat = $_POST['post-cat'];
 $_SESSION['cat'] = $cat;
 $_SESSION['nomProd'] = $prodNom;
 
+$p = $db->prepare('SELECT * FROM category WHERE categoryName = :catNom');
+$p->bindValue(':catNom', $cat);
+$p->execute();
+$catFetch = $p->fetch(PDO::FETCH_ASSOC);
+$catImgDefault = $catFetch["defaultImg"];
 
 $uploadDir = "../../assets/img/imgProducts/";
 $idUnique = uniqid().date("Y-m-d-h-i-s");
 $uploadFile = $uploadDir.$idUnique.$_POST['post-img'];
 $photo = $uploadDir.$idUnique.$_POST['post-img'];
 
+$photoOrDefault = ($_POST['post-img'] == null) ? $catImgDefault : $photo;
 
 move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFile );
 
@@ -32,7 +38,7 @@ move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFile );
 $q = $db->prepare('INSERT INTO product(productName, productImg, description, creationDate, creator)'
             . ' VALUES(:productName, :productImg, :description, :creationDate, :creator)');
     $q->bindValue(':productName', $prodNom);
-    $q->bindValue(':productImg', $photo);
+    $q->bindValue(':productImg', $photoOrDefault);
     $q->bindValue(':description', $desc);
     $q->bindValue(':creationDate', date("Y-m-d"));
     $q->bindValue(':creator', $creator);
