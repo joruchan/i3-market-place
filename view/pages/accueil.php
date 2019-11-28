3c876ac158d69927e4dbd5e4fd30c9c193273017
Alexpalarus <alexandra.palarus@gmail.com>
2019-11-28 14:31:13 +0100

    Home page

--- view/pages/accueil.php
+++ view/pages/accueil.php
@@ -7,11 +7,11 @@ include '../partials/head.php';
     <img id="logo" src="../../assets/img/Logo.png" alt="">
 
         <form action="#" method="POST" class="row" id="login">
-            <div class="input-field col s6">
+            <div class="input-field">
             <input id="user-name" type="text" name="user-name" class="validate">
             <label for="user-name">User Name</label>
             </div>
-            <div class="input-field col s6">
+            <div class="input-field">
                 <input id="user-password" type="password" name="user-password" class="validate">
                 <label for="user-password">Password</label>
             </div>
