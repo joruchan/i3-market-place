aef058eda1a0bc575e5d4ee8ca9a477115411876
cerise <carnetsdecerise@gmail.com>
2019-11-29 14:41:44 +0100

    new DB

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
     define('DBDRIVER','mysql');
     define('DBHOST','localhost');
-    define('DBPORT',3306);
-    define('DBNAME','i3-market');
+    define('DBPORT',3308);
+    define('DBNAME','i3_gift_box');
     define('DBCHARSET','utf8');
     define('DBUSER','root');
     define('DBPASS','');
