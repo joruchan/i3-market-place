1f4bcd679ec05a20f9deff536e002c020769de07
Jordane <jordane.frechet@gmail.com>
2019-11-29 09:52:51 +0100

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ import {createObjectCardInit, createCategorySelect} from "./app/createComponents
 
 createObjectCardInit();
 createCategorySelect();
-
+alert("<?php echo 'COUCOU' . $_SESSION['id'] ?>");
 $(document).ready(function(){
 	$('select').formSelect();
 });
+
--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
     define('DBDRIVER','mysql');
     define('DBHOST','localhost');
     define('DBPORT',3306);
-    define('DBNAME','i3-market');
+    define('DBNAME','i3_gift_box');
     define('DBCHARSET','utf8');
     define('DBUSER','root');
     define('DBPASS','');
