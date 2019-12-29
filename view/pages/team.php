a792faaf399b72c366f8fb4d80c2f1c928e7525f
vdestrait <valentinedestrait@hotmail.com>
2019-12-29 14:38:19 +0100

    Update team.php

--- view/pages/team.php
+++ view/pages/team.php
@@ -7,31 +7,24 @@ include '../partials/header.php';
     <div class="container">
         <h1>Team</h1>
             <div class="row">
-            <figure class="col s12 m6 l3">
+            <figure class="col s12 m6 l4">
                 <img src="../../assets/img/Alex.png" alt="">
                 <figcaption>
                     <p>Alexandra</p>
                     <p class="i3-section">Game</p>
                 </figcaption>
             </figure>
-            <figure class="col s12 m6 l3">
+            <figure class="col s12 m6 l4">
                 <img src="../../assets/img/Cerise.png" alt="">
                 <figcaption>
                     <p>Cerise</p>
                     <p class="i3-section">WAD</p>
                 </figcaption>
             </figure>
-            <figure class="col s12 m6 l3">
+            <figure class="col s12 m6 l4">
                 <img src="../../assets/img/Jordane.png" alt="">
                 <figcaption>
                     <p>Jordane</p>
-                    <p class="i3-section">Web</p>
-                </figcaption>
-            </figure>
-            <figure class="col s12 m6 l3">
-                <img src="../../assets/img/Valentine.png" alt="">
-                <figcaption>
-                    <p>Valentine</p>
                     <p class="i3-section">Web</p>
                 </figcaption>
             </figure>
